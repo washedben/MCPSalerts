d5a3760cf00772b3c61b69e48a4e78194d600e9a
washedben <46820112+washedben@users.noreply.github.com>
2019-01-29 20:19:06 -0500

    Update mcpsv2.py

--- mcpsv2.py
+++ mcpsv2.py
@@ -4,21 +4,21 @@ import smtplib
 from time import sleep
 import sys
 
-print (" ----------------------------------------------")
-print ("                MCPS TEXT ALERTS     ")
-print ("           Developed by Ben @dontwrk           ")
-print (" ----------------------------------------------")
 
+print (" ----------------------------------------------")
+print ("                MCPS TEXT ALERTS               ")
+print ("                Developed by Ben                    ")
+print (" ----------------------------------------------")
 
 provider = 'smtp.gmail.com' #IGNORE
 port = 587 #IGNORE
 phone_num = str(input("Number to Notify: "))
 gmail_address = str(input("Enter Gmail Address: "))
-password = str(input("Enter Gmail Password: ")) 
-msg1 = 'School cancelled lets go!!!' 
+password = str(input("Enter Gmail Password: "))
+msg1 = 'School cancelled lets go!!!'
 msg2 = 'School delayed lets go!!!'
 text_amount = 3 #IGNORE
-carrier = str(input("What Carrier do you have? (tmobile, sprint, verizon, att): ")) 
+carrier = str(input("What Carrier do you have? (tmobile, sprint, verizon, att): "))
 
 
 
@@ -30,6 +30,7 @@ print('Thanks! We will notify when a change of school schedule occurs.')
 
 
 carrier_email = ""
+target_email = phone_num+"@"+carrier_email
 
 if carrier == "tmobile":
 	carrier_email = "tmomail.net"
@@ -38,30 +39,31 @@ elif carrier == 'sprint':
 elif carrier == "att":
 	carrier_email = "txt.att.net"
 elif carrier == "verizon":
-	carrier_email = "vtext.com"	
+	carrier_email = "vtext.com"
 else:
-	print("Entered Carrier Wrong!")	
-    
+	print("Entered Carrier Wrong!")
+
+
 
 def sms(message):
-    carrier_email = ""
+    carrier_email = "vtext.com"
     target_email = phone_num+"@"+carrier_email
     wait = 2 #DELAY
     server = smtplib.SMTP(provider, port)
     server.starttls()
     server.login(gmail_address, password)
     for _ in range(0,text_amount):
-        server.sendmail(gmail_address,target_email,message)
-        print("Sent "+phone_num+" - "+message)
+        server.sendmail(gmail_address,target_email,msg2)
+        print("Sent "+phone_num+" - "+msg2)
         time.sleep(wait)
-        
     server.quit()
 
+
 while True:
-    r = requests.get('https://montgomeryschoolsmd.org').text.count('delayed') #Can be edited with other website for other school
+    c = requests.get('https://montgomeryschoolsmd.org').text.count('delayed') #Can be edited with other website for other school
     d = requests.get('https://montgomeryschoolsmd.org').text.count('closed') #Same as here
-   
-    if r > 0:
+
+    if c > 0:
         print('School delayed! Sending texts...')
         sms(msg2)
         print("{} texts were sent! -  happy sleeping".format(text_amount))
